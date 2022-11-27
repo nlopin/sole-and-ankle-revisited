672b4e10cec349f9f1a054c56bae38c9064f5b2d
Nikolai Lopin <code@lopin.me>
2022-11-27 17:11:36 +0100

    adds mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { QUERIES } from '../../constants';
+import { COLORS } from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
@@ -15,23 +15,86 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <DialogOverlay as={Overlay} isOpen={isOpen} onDismiss={onDismiss}>
+      <DialogContent as={Content}>
+        <CloseAction>
+          <UnstyledButton onClick={onDismiss}>
+            <Icon id="close" strokeWidth={2}/>
+            <VisuallyHidden>Dismiss menu</VisuallyHidden>
+          </UnstyledButton>
+        </CloseAction>
+        <Menu>
+          <MenuLink style={{'--color': COLORS.secondary }} href="/sale">Sale</MenuLink>
+          <MenuLink href="/new">New&nbsp;Releases</MenuLink>
+          <MenuLink href="/men">Men</MenuLink>
+          <MenuLink href="/women">Women</MenuLink>
+          <MenuLink href="/kids">Kids</MenuLink>
+          <MenuLink href="/collections">Collections</MenuLink>
+        </Menu>
+        <Footer>
+          <FooterLinks href="/terms">Terms and Conditions</FooterLinks>
+          <FooterLinks href="/privacy">Privacy Policy</FooterLinks>
+          <FooterLinks href="/contact">Contact Us</FooterLinks>
+        </Footer>
+      </DialogContent>
+    </DialogOverlay>
   );
 };
+
+const Overlay = styled.div`
+  position: absolute;
+  width: 100%;
+  min-height: 100vh;
+  left: 0;
+  top: 0;
+
+  background: rgba(96, 100, 108, 0.8);
+`
+
+const Content = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  width: calc(300/16 * 1rem);
+  min-height: 100vh;
+  top: 0;
+  right: 0;
+  background: ${COLORS.white};
+  padding: 32px;
+  padding-right: 22px;
+`
+
+const CloseAction = styled.div`
+  align-self: flex-end;
+`
+
+const Menu = styled.nav`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+  justify-content: center;
+`
+
+const MenuLink = styled.a`
+  text-decoration: none;
+  font-weight: 600;
+  font-size: calc(18 / 16 * 1rem);
+  text-transform: uppercase;
+  color: var(--color, ${COLORS.gray['900']});
+`
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`
+
+const FooterLinks = styled.a`
+  color: ${COLORS.gray['700']};
+  text-decoration: none;
+  font-size: calc(14 / 16 * 1rem);
+  font-weight: 500;
+`
 
 export default MobileMenu;
